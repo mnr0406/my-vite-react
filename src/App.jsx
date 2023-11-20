feeeda600b1640296c5663fec745600da30dd80e
Musbahniar <ruang.9campus@gmail.com>
2023-11-20 10:57:20 +0700

    first-commit-vite-react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="flex bg-blue-100">
+        <h1 className="text-3xl font-bold underline">Musbahniar</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
